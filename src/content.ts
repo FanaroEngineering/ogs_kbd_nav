2c323c9a2664d636b20163c9b329d2a8664b53c7
Philippe Fanaro <philippefanaro@gmail.com>
2020-11-03 10:08:43 -0300

    added (un)focus of the chat input

--- src/content.ts
+++ src/content.ts
@@ -1,10 +1,34 @@
-const chatInputQuery: string = 'div.chat-container > div > input.main';
+class KbddEvtHandler {
+  private kbdEvt: KeyboardEvent;
 
-const chatInput: HTMLInputElement = document.querySelector(
-  chatInputQuery
-) as HTMLInputElement;
+  constructor() {
+    this.kbdEvt = new KeyboardEvent("keypress");
+    document.onkeydown = (evt: KeyboardEvent) => {
+      this.kbdEvt = evt;
+      this._keySwitch();
+    };
+  }
 
-document.onkeydown = (evt: KeyboardEvent) => {
-  console.log(evt.key);
-  console.log('here');
-};
+  _keySwitch(): void {
+    switch (this.kbdEvt.key) {
+      case "m":
+        this._toggleChatInput();
+        break;
+    }
+  }
+
+  _toggleChatInput(): void {
+    const chatInputQuery: string = "div.chat-container > div > input.main";
+    const chatInput: HTMLInputElement = document.querySelector(
+      chatInputQuery
+    ) as HTMLInputElement;
+
+    if (this.kbdEvt.ctrlKey) {
+      document.activeElement == chatInput
+        ? chatInput.blur()
+        : chatInput.focus();
+    }
+  }
+}
+
+const kbddEvtHandler = new KbddEvtHandler();
