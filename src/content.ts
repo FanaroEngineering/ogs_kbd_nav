9963d203ba48efd2f4846825c35dd56d66186904
Philippe Fanaro <philippefanaro@gmail.com>
2020-11-03 23:20:07 -0300

    first attempt at testing with jest

--- src/content.ts
+++ src/content.ts
@@ -1,3 +1,7 @@
-import Kbd from "./kbd.js";
+import Kbd from "./kbd";
+import StoneMarker from "./stone_marker";
+
+const stone = new StoneMarker();
+stone.draw(100, 100);
 
 new Kbd();
--- src/stone_marker.ts
+++ src/stone_marker.ts
@@ -2,11 +2,13 @@ export default class StoneMarker {
   constructor(public sideLength: number = 10) {}
 
   draw(x: number, y: number): void {
-    const canvas: HTMLCanvasElement = document.querySelector('canvas.StoneLayer') as HTMLCanvasElement;
-    const context: CanvasRenderingContext2D = canvas.getContext('2d')!;
+    const canvas: HTMLCanvasElement = document.querySelector(
+      "canvas.StoneLayer"
+    ) as HTMLCanvasElement;
+    const context: CanvasRenderingContext2D = canvas.getContext("2d")!;
 
     context.beginPath();
-    context.arc(x, y, 10, 0, 2 * Math.PI, false);
+    context.arc(x, y, this.sideLength, 0, 2 * Math.PI, false);
     context.lineWidth = 5;
     context.strokeStyle = "black";
     context.stroke();
