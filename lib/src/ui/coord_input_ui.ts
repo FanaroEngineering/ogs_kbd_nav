2cf9e25fb811c86096303541fbdce94344995e55
Philippe Fanaro <philippefanaro@gmail.com>
2020-11-19 14:46:29 -0300

    validation for the coordinates input field

--- lib/src/ui/coord_input_ui.ts
+++ lib/src/ui/coord_input_ui.ts
@@ -9,14 +9,26 @@ export default class CoordInputUi {
     ) as HTMLDivElement;
 
     this.style();
+    this.builtInValidation();
+    this.coordInput.onchange = this.onChange;
 
     gobanDiv.append(this.coordInput);
   }
 
   private style = (): void => {
+    this.coordInput.id = "coordinates";
+    this.coordInput.type = "text";
+    this.coordInput.placeholder = "Coordinates";
+    this.coordInput.size = this.coordInput.placeholder.length;
     this.coordInput.style.position = "absolute";
     this.coordInput.style.bottom = "0";
     this.coordInput.style.borderColor = "green";
+  };
+
+  private builtInValidation = (): void => {
+    this.coordInput.minLength = 2;
+    this.coordInput.maxLength = 3;
+    this.coordInput.pattern = "[A-T|a-t][0-1]?[0-9]";
   };
 
   focus = (): void => this.coordInput.focus();
@@ -24,4 +36,16 @@ export default class CoordInputUi {
   get isFocused(): boolean {
     return document.activeElement == this.coordInput;
   }
+
+  private onChange = (_: Event): any => {
+    if (this.coordInput.validity.patternMismatch) {
+      this.coordInput.setCustomValidity(
+        `A coordinate is composed by 
+         a letter from A to T (upper or lowercase) 
+         and a number from 1 to 19.`
+      );
+    } else {
+      this.coordInput.setCustomValidity("");
+    }
+  };
 }
