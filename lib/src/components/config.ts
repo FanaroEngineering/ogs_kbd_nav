5e7fab45ed47a5fa46572a6bfa95653ef141f0a5
Philippe Fanaro <philippefanaro@gmail.com>
2020-11-27 18:59:30 -0300

    added cycling size to the configs

--- lib/src/components/config.ts
+++ lib/src/components/config.ts
@@ -1,5 +1,14 @@
+export enum GobanSize {
+  small9x9,
+  medium13x13,
+  full19x19,
+}
+
 export default class Config {
-  private constructor(private readonly _arrowKeysOn: boolean = false) {}
+  private constructor(
+    private readonly _arrowKeysOn: boolean = false,
+    private readonly _gobanSize: GobanSize = GobanSize.full19x19
+  ) {}
 
   static default = (): Config => new Config();
 
@@ -7,5 +16,22 @@ export default class Config {
     return this._arrowKeysOn;
   }
 
-  toggleArrowKeys = (): Config => new Config(!this._arrowKeysOn);
+  get gobanSize(): GobanSize {
+    return this._gobanSize;
+  }
+
+  toggleArrowKeys = (): Config =>
+    new Config(!this._arrowKeysOn, this._gobanSize);
+
+  cycleSize = (): Config => {
+    let nextSizeIndex: number;
+    if (this._gobanSize == 2) nextSizeIndex = 0;
+    else nextSizeIndex = this._gobanSize + 1;
+
+    const nextSize: GobanSize = Object.values(GobanSize).indexOf(
+      Object.values(GobanSize)[nextSizeIndex]
+    );
+
+    return new Config(this._arrowKeysOn, nextSize);
+  };
 }
--- lib/tests/config.test.ts
+++ lib/tests/config.test.ts
@@ -1,9 +1,10 @@
-import Config from "../src/components/config";
+import Config, { GobanSize } from "../src/components/config";
 
-test("Initially Config.arrowKeysOn is false", () => {
+test("Initially Configs", () => {
   const config: Config = Config.default();
 
   expect(config.arrowKeysOn).toBe(false);
+  expect(config.gobanSize).toBe(GobanSize.full19x19);
 });
 
 test("Toggling the arrow keys", () => {
@@ -14,4 +15,24 @@ test("Toggling the arrow keys", () => {
   const toggledConfig: Config = config.toggleArrowKeys();
 
   expect(toggledConfig.arrowKeysOn).toBe(true);
+  expect(toggledConfig.gobanSize).toBe(GobanSize.full19x19);
+});
+
+test("Cycling the goban size", () => {
+  const config: Config = Config.default();
+
+  let cycledConfig: Config = config.cycleSize();
+
+  expect(cycledConfig.arrowKeysOn).toBe(false);
+  expect(cycledConfig.gobanSize).toBe(GobanSize.small9x9);
+
+  cycledConfig = cycledConfig.cycleSize();
+
+  expect(cycledConfig.arrowKeysOn).toBe(false);
+  expect(cycledConfig.gobanSize).toBe(GobanSize.medium13x13);
+
+  cycledConfig = cycledConfig.cycleSize();
+
+  expect(cycledConfig.arrowKeysOn).toBe(false);
+  expect(cycledConfig.gobanSize).toBe(GobanSize.full19x19);
 });
